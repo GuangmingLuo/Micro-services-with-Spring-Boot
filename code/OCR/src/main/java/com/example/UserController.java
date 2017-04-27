27fdd80f6f5498be2ad6c21e385c839694637f11
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-04-27 12:55:43 +0200

    redirect to the correponding restaurant page if logged in

--- code/OCR/src/main/java/com/example/UserController.java
+++ code/OCR/src/main/java/com/example/UserController.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import com.example.entities.Restaurant;
+import com.example.services.RestaurantService;
+import com.example.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +18,20 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private RestaurantService restaurantService;
+
     @RequestMapping(value= "/login",method=GET)
     public String login() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(!(auth.getPrincipal().equals("anonymousUser"))) {
+            org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) auth.getPrincipal();
+            com.example.entities.User userExists = userService.findByUsername(user.getUsername());
+            Restaurant rest = restaurantService.findRestaurantById(userExists.getRestaurantId());
+            return "redirect:/restaurant/"+rest.getName()+"/";
+        }
         return "login";
     }
 
