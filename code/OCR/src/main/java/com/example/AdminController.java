90838f71da7d0f6b007b9710a6565ba795cb25bc
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-04-27 12:03:18 +0200

    after register post, redirect to registration to avoid resubmit form. Using flashAttribute to pass message from post function.

--- code/OCR/src/main/java/com/example/AdminController.java
+++ code/OCR/src/main/java/com/example/AdminController.java
@@ -10,9 +10,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -32,26 +34,25 @@ public class AdminController {
     private RestaurantService restaurantService;
     private static final Logger log = LoggerFactory.getLogger(AdminController.class);
 
-    @RequestMapping(value= "/register",method= RequestMethod.GET)
-    public String register(@RequestParam(value="message",required = false,defaultValue = "") String message,Model model) {
+    @RequestMapping(value= "/registration",method= RequestMethod.GET)
+    public String register(@ModelAttribute("message") final String message, Model model) {
         model.addAttribute("message",message);
         List<Restaurant> restaurants = restaurantService.findAll();
-        //.addAttribute(new User());
         model.addAttribute("restaurants",restaurants);
         return "register";
     }
 
     @RequestMapping(value="/register",method= RequestMethod.POST)
-    public String registerPost(@Valid User user, @Valid int restaurant_id, Model model) {
+    public String registerPost(@Valid User user, @Valid int restaurant_id, RedirectAttributes redir) {
         user.setRestaurantId(restaurant_id);
         log.info("username is {} and password is {}, restaurant id is {}",user.getUsername(),user.getPassword(),user.getRestaurantId());
         User userExists = userService.findByUsername(user.getUsername());
         if(userExists !=null){
-            model.addAttribute("message"," This username has been registered!");
+            redir.addFlashAttribute("message"," This username has been registered!");
         }else{
-            model.addAttribute("message"," Success!");
+            redir.addFlashAttribute("message"," Success!");
             userService.saveUser(user);
         }
-        return "register";
+        return "redirect:/admin/registration";
     }
 }
