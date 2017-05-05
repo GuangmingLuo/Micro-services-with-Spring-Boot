5304a3c93d2bfeec554ebb8b7f3aa664f08a37f2
Lennart Cockx <lennart.cockx@gmail.com>
2017-05-05 14:19:34 +0200

    Added tomcat server configuration section to application.properties

--- code/OCR/src/main/java/com/example/api/APIController.java
+++ code/OCR/src/main/java/com/example/api/APIController.java
@@ -10,6 +10,7 @@ import com.example.services.FoodService;
 import com.example.services.MenuService;
 import com.example.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +28,16 @@ public class APIController {
     @Autowired
     private FoodService foodService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
+    @RequestMapping
+    public String info() {
+        return "/menu?restaurantId=X";
+    }
+
     /*
     * This api returns a restaurant entity by a restaurant name
     * */
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/restaurant")
     public Restaurant restaurant(@RequestParam(value="name") String name) {
         return restaurantService.findRestaurantByName(name);
@@ -37,6 +45,7 @@ public class APIController {
     /*
     * This api returns all restaurant entities
     * */
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/restaurants")
     public List<Restaurant> restaurants() {
         return restaurantService.findAll();
@@ -44,6 +53,7 @@ public class APIController {
     /*
     * This api returns a list of menu entities by a restaurant id
     * */
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/menu")
     public List<Menu> menu(@RequestParam(value="restaurantId") int restaurantId) {
         List<Menu> menus = menuService.findMenuByRestaurantId(restaurantId);
