3b2a7b067154f72725e3eb895df2e3a01c9ce4a1
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-05-11 11:37:03 +0200

    Create an Entities project to generate a jar file.

---
+++ code/Multi-Services/Entities/src/UserRole.java
@@ -0,0 +1,43 @@
+/**
+ * Created by guang on 2017/4/21.
+ */
+public class UserRole {
+    private int userId;
+    private int roleId;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRole userRole = (UserRole) o;
+
+        if (userId != userRole.userId) return false;
+        if (roleId != userRole.roleId) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId;
+        result = 31 * result + roleId;
+        return result;
+    }
+}
