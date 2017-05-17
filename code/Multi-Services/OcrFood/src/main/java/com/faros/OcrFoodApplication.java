e5e035048a040efd0a5e146137b5559f4f795b38
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-05-17 11:02:43 +0200

    config OcrMenu, OcrFood, OcrOrder as eureka server.

--- code/Multi-Services/OcrFood/src/main/java/com/faros/OcrFoodApplication.java
+++ code/Multi-Services/OcrFood/src/main/java/com/faros/OcrFoodApplication.java
@@ -2,8 +2,10 @@ package com.faros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OcrFoodApplication {
 
 	public static void main(String[] args) {
