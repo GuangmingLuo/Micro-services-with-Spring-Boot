0f5d5a63f0579a54ffeceae8717b11359cc7b86e
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-05-17 12:51:27 +0200

    config OcrGateway with eureka service enabled

--- code/Multi-Services/OcrGateway/src/main/java/com/faros/OcrGateway/OcrGatewayApplication.java
+++ code/Multi-Services/OcrGateway/src/main/java/com/faros/OcrGateway/OcrGatewayApplication.java
@@ -2,13 +2,25 @@ package com.faros.OcrGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class OcrGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OcrGatewayApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 }
