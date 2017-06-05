d0b6e4c4fcf2486565eb6108c3646e882bbb6dd4
DESKTOP-55K2RBN\guang <guangmingluo1994@gmail.com>
2017-06-05 16:00:17 +0200

    Add comments to all the methods of all the services in OcrClient application.

--- code/Multi-Services/OcrClient/src/main/java/com/faros/services/OrderService.java
+++ code/Multi-Services/OcrClient/src/main/java/com/faros/services/OrderService.java
@@ -10,8 +10,23 @@ import java.util.List;
  * Created by guang on 2017/5/8.
  */
 public interface OrderService {
+    /*
+    * This method is used to save an order entity
+    * */
     void save(Order order);
+
+    /*
+    * This method is used to check if order api available or not
+    * */
     boolean checkApiStatus();
+
+    /*
+    * This method is used to find all the available orders by restaurantId
+    * */
     List<JSONObject> findAll(int restaurantId) throws IOException;
+
+    /*
+    * This method is used to change the status of an order by id and newStatus
+    * */
     void changeOrderStatus(long id, String newStatus);
 }
